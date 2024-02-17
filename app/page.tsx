3ed89cb382e3a8f28c9992cb7c400daaf811a418
BestVersion7 <hunterkfan@gmail.com>
2024-02-17 15:46:01 -0500

    add back video

--- app/page.tsx
+++ app/page.tsx
@@ -40,13 +40,13 @@ export default async function Home() {
                 <AsideNav modal={false} />
             </aside>
 
-            {/* <div className="max-w-[2000px] m-auto px-4 sm:px-6">
+            <div className="max-w-[2000px] m-auto px-4 sm:px-6">
                 <div className=" md:ml-16 xl:ml-64 grid gap-x-4  gap-y-7 sm:grid-cols-2 lg:grid-cols-3 min-[1400px]:grid-cols-4 min-[1720px]:grid-cols-5">
                     {videos.items.map((item, index) => (
                         <VideoPreview key={index} {...item} />
                     ))}
                 </div>
-            </div> */}
+            </div>
         </main>
     );
 }
